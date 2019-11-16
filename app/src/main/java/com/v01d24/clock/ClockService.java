3f4c79ceaef37c758f849c93b5c7180c660b8e09
Ozhegov Anton <eternal.void.24@gmail.com>
2019-11-16 22:55:40 +0300

    disable time listener when screen is off

--- app/src/main/java/com/v01d24/clock/ClockService.java
+++ app/src/main/java/com/v01d24/clock/ClockService.java
@@ -3,12 +3,43 @@ package com.v01d24.clock;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.*;
+import android.graphics.Bitmap;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 public class ClockService extends Service {
 
     private static final String TAG = ClockService.class.getSimpleName();
+
+    private static final String ACTION_UPDATE_WIDGET = "UPDATE_WIDGET";
+    private static final String ACTION_RESIZE_WIDGET = "RESIZE_WIDGET";
+
+    private static final String ARG_WIDGET_WIDTH = "WIDGET_WIDTH";
+    private static final String ARG_WIDGET_HEIGHT = "WIDGET_HEIGHT";
+
+    private BroadcastReceiver screenOnOffReceiver;
+    private BroadcastReceiver timeChangedReceiver;
+
+    private ClockBitmapGenerator bitmapGenerator;
+
+    static void updateWidget(Context context) {
+        Intent intent = new Intent(context, ClockService.class);
+        intent.setAction(ACTION_UPDATE_WIDGET);
+        context.startService(intent);
+    }
+
+    static void resizeWidget(Context context, int width, int height) {
+        Intent intent = new Intent(context, ClockService.class);
+        intent.setAction(ACTION_RESIZE_WIDGET);
+        intent.putExtra(ARG_WIDGET_WIDTH, width);
+        intent.putExtra(ARG_WIDGET_HEIGHT, height);
+        context.startService(intent);
+    }
+
+    static void stop(Context context) {
+        context.stopService(new Intent(context, ClockService.class));
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -18,41 +49,106 @@ public class ClockService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.e(TAG, "ClockService.onCreate");
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_TIME_TICK);
-        filter.addAction(Intent.ACTION_TIME_CHANGED);
-        filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-        registerReceiver(timeChangedReceiver, filter);
+        Log.e(TAG, "onCreate");
+
+        Settings settings = new Settings(this);
+        int width = settings.getWidgetWidth();
+        int height = settings.getWidgetHeight();
+        bitmapGenerator = new ClockBitmapGenerator(width, height);
+
+        registerScreenOnOffReceiver();
+        registerTimeChangedReceiver();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        Log.e(TAG, "ClockService.onStartCommand");
+        String action = intent.getAction();
+        Log.e(TAG, "onStartCommand action=" + action);
+
+        if (ACTION_UPDATE_WIDGET.equals(action)) {
+            updateWidget();
+        }
+        else if (ACTION_RESIZE_WIDGET.equals(action)) {
+            int width = intent.getIntExtra(ARG_WIDGET_WIDTH, 0);
+            int height = intent.getIntExtra(ARG_WIDGET_HEIGHT, 0);
+            if (width > 0 && height > 0) {
+                new Settings(this).setWidgetSize(width, height);
+                bitmapGenerator.setSize(width, height);
+                updateWidget();
+            }
+        }
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.e(TAG, "ClockService.onDestroy");
-        unregisterReceiver(timeChangedReceiver);
+        Log.e(TAG, "onDestroy");
+        unregisterScreenOnOffReceiver();
+        unregisterTimeChangedReceiver();
     }
 
-    private BroadcastReceiver timeChangedReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.e(TAG, "timeChangedReceiver.onReceive");
-            updateWidget();
+    private void registerScreenOnOffReceiver() {
+        if (screenOnOffReceiver == null) {
+            screenOnOffReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    boolean screenOff = Intent.ACTION_SCREEN_OFF.equals(intent.getAction());
+                    Log.e(TAG, "screenOnOffReceiver.onReceive screenOff=" + screenOff);
+                    if (screenOff) {
+                        unregisterTimeChangedReceiver();
+                    }
+                    else {
+                        updateWidget();
+                        registerTimeChangedReceiver();
+                    }
+                }
+            };
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            registerReceiver(screenOnOffReceiver, filter);
         }
-    };
+    }
+
+    private void unregisterScreenOnOffReceiver() {
+        if (screenOnOffReceiver != null) {
+            unregisterReceiver(screenOnOffReceiver);
+            screenOnOffReceiver = null;
+        }
+    }
+
+    private void registerTimeChangedReceiver() {
+        if (timeChangedReceiver == null) {
+            timeChangedReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Log.e(TAG, "timeChangedReceiver.onReceive");
+                    updateWidget();
+                }
+            };
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_TIME_TICK);
+            filter.addAction(Intent.ACTION_TIME_CHANGED);
+            filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+            registerReceiver(timeChangedReceiver, filter);
+        }
+    }
+
+    private void unregisterTimeChangedReceiver() {
+        if (timeChangedReceiver != null) {
+            unregisterReceiver(timeChangedReceiver);
+            timeChangedReceiver = null;
+        }
+    }
 
     private void updateWidget() {
+        RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget);
+        Bitmap bitmap = bitmapGenerator.generate();
+        views.setBitmap(R.id.image, "setImageBitmap", bitmap);
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, WidgetProvider.class));
-        if (appWidgetIds.length > 0) {
-            new WidgetProvider().onUpdate(this, appWidgetManager, appWidgetIds);
-        }
+        ComponentName provider = new ComponentName(this, WidgetProvider.class);
+        appWidgetManager.updateAppWidget(provider, views);
     }
 }
--- app/src/main/java/com/v01d24/clock/WidgetProvider.java
+++ app/src/main/java/com/v01d24/clock/WidgetProvider.java
@@ -3,25 +3,20 @@ package com.v01d24.clock;
 import android.annotation.TargetApi;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.*;
-import android.graphics.Bitmap;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider {
 
     private static final String TAG = WidgetProvider.class.getSimpleName();
 
-    private static ClockBitmapGenerator bitmapGenerator;
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         Log.e(TAG, "onUpdate");
-        updateWidgets(context, appWidgetManager, appWidgetIds);
-        context.startService(new Intent(context, ClockService.class));
+        ClockService.updateWidget(context);
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -30,34 +25,14 @@ public class WidgetProvider extends AppWidgetProvider {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
         int width = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
         int height = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
-        Log.e(TAG, "onAppWidgetOptionsChanged " + width + " " + height);
-        new Settings(context).setWidgetSize(width, height);
-        if (bitmapGenerator != null) {
-            bitmapGenerator.setSize(width, height);
-        }
-        updateWidgets(context, appWidgetManager, new int[]{ appWidgetId });
+        Log.e(TAG, "onAppWidgetOptionsChanged width=" + width + " height=" + height);
+        ClockService.resizeWidget(context, width, height);
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
         Log.e(TAG, "onDeleted");
-        context.stopService(new Intent(context, ClockService.class));
-    }
-
-    private void updateWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        if (bitmapGenerator == null) {
-            Settings settings = new Settings(context);
-            int width = settings.getWidgetWidth();
-            int height = settings.getWidgetHeight();
-            bitmapGenerator = new ClockBitmapGenerator(width, height);
-            Log.e(TAG, "Create bitmap generator " + width + " " + height);
-        }
-        Bitmap bmp = bitmapGenerator.generate();
-        for (int appWidgetId: appWidgetIds) {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-            views.setBitmap(R.id.image, "setImageBitmap", bmp);
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+        ClockService.stop(context);
     }
 }
